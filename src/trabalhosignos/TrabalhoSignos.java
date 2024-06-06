1da303ad06274432ce24d9ca6cf3fa1107a0bac1
thain <thain@Thain>
2024-06-05 22:26:43 -0300

    Projeto Signos, primeiro commit Desenvolvido por: Thainá Tavares

--- src/trabalhosignos/TrabalhoSignos.java
+++ src/trabalhosignos/TrabalhoSignos.java
@@ -8,9 +8,9 @@ import java.util.Scanner;
 public class TrabalhoSignos {
 
     public static void main(String[] args) {
-        String nome, caracteres, cor;
+        String nome;
         Calendar hoje = Calendar.getInstance();
-        int dia, mes, ano, idade = 0, signo, sexo;
+        int dia, mes, ano, idade = 0, signo, sexo, cor = 0;
         int diaA, mesA, anoA;
         diaA = hoje.get(Calendar.DATE);
         mesA = hoje.get(Calendar.MONTH) + 1;
@@ -29,7 +29,7 @@ public class TrabalhoSignos {
             return;
         }
         String carateres = "1234567890!@#$%¨&*()-_=+§{[ª~^|}]'º:;?/°,.\\\"";
-        if (nome.equals(carateres)) {
+        if (nome != carateres) {
 
         } else {
             System.out.println("Seu nome só deve ter letras");
@@ -38,14 +38,6 @@ public class TrabalhoSignos {
 
         System.out.println("Qual é dia que você nasceu?");
         dia = ler.nextInt();
-
-        if (dia >= 1 && dia <= 31) {
-
-        } else {
-            System.out.println("Informe um dia entre 1 a 31");
-
-            return;
-        }
 
         System.out.println("Qual o mês que você nasceu?");
         mes = ler.nextInt();
@@ -65,20 +57,19 @@ public class TrabalhoSignos {
             return;
         }
 
-        //validade do nascimento, dia, mês e ano
-        if ((mes == 4 || mes = 6 || mes == 9 || mes == 11) && dia > 30) {
-            System.out.println("O mês informado só tem 30 dias");
+        if ((mes == 4 || mes == 6 || mes == 9 || mes == 11) && dia > 30) {
+            System.out.println("Esse mês só possui 30 dias");
             return;
-        } else if ((mes = 2 && (ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0))  {
-            if (dia > 29) {
-                System.out.println("Fevereiro em ano Bissexto só tem 29 dias");
-                return;
-            }
-        } else if (mes == 2 && dia > 28) {
+
+                    }else if (mes ==2 &&  (ano%4==0 && ano %100!=0)|| (ano%400==0)){
+        System.out.println("Fevereiro em ano Bissexto tem só 29 dias");
+        return;
+
+        } else if (mes == 2 && dia <= 28) {
             System.out.println("Fevereiro só tem 28 dias");
             return;
         }
-
+        
         System.out.println("Qual é o seu sexo 1-Feminino ou 2-Masculino");
         sexo = ler.nextInt();
 
@@ -104,19 +95,18 @@ public class TrabalhoSignos {
         // RESULTADO
         System.out.println("Você é do signo " + gerarSigno(dia, mes, ano) + " você tem " + idade + " anos de idade");
 
-        System.out.println("Seus números da sorte são " + declararNumeroSorte(gerarSigno(dia, mes, ano)));
-
+        System.out.println("Seus números da sorte são " + declararNumeroSorte(gerarSigno(dia, mes, ano)) + " e sua cor é " + declararCor(nome));
     }
-//cor aleatória
 
-    public String declararCor(String cores) {
+    //cor aleatória
+    public static String declararCor(String cor) {
 
         String corAleatoria[] = {"vermelho-intenso", "verde-aconchegante", "amarelo-vivo", "rosa-delicado", "laranja-expressivo ", "branco-pigmentado", "violeta-silencioso", "marrom-avermelhado", "magenta "};
         Random nCor = new Random();
         int i = nCor.nextInt(corAleatoria.length);
-        cores = corAleatoria[i];
+        cor = corAleatoria[i];
 
-        return cores;
+        return cor;
 
     }
 
@@ -124,10 +114,16 @@ public class TrabalhoSignos {
     public static String gerarSigno(int dia, int mes,
             int ano) {
         String signo = "";
-        if (dia >= 21 && dia <= 30 && mes == 6 || dia >= 1 && dia <= 20 && mes == 7) {
+        if (dia >= 21 && dia <= 31 && mes == 3 || dia >= 1 && dia <= 20 && mes == 4) {
+            signo = "Áries";
+        } else if (dia >= 21 && dia <= 30 && mes == 4 || dia >= 1 && dia <= 20 && mes == 5) {
+            signo = "Touro";
+        } else if (dia >= 21 && dia <= 31 && mes == 5 || dia >= 1 && dia <= 20 && mes == 6) {
+            signo = "Gêmeos";
+        } else if (dia >= 21 && dia <= 30 && mes == 6 || dia >= 1 && dia <= 20 && mes == 7) {
             signo = "Câncer";
         } else if (dia >= 22 && dia <= 31 && mes == 7 || dia >= 1 && dia <= 22 && mes == 8) {
-            signo = " Leão";
+            signo = "Leão";
         } else if (dia >= 23 && dia <= 31 && mes == 8 || dia >= 1 && dia <= 22 && mes == 9) {
             signo = "Virgem";
         } else if (dia >= 23 && dia <= 30 && mes == 9 || dia >= 1 && dia <= 22 && mes == 10) {
@@ -140,40 +136,40 @@ public class TrabalhoSignos {
             signo = "Capricórnio";
         } else if (dia >= 23 && dia <= 31 && mes == 1 || dia >= 1 && dia <= 19 && mes == 2) {
             signo = "Aquário";
-        } else if (dia >= 20 && dia <= 28 && mes == 2 || dia >= 1 && dia <= 20 && mes == 3) {
+        } else if (dia >= 20 && dia <= 29 && mes == 2 || dia >= 1 && dia <= 20 && mes == 3) {
             signo = "Peixes";
         }
 
         return signo;
     }
-
     //número da sorte de acordo com o signo
+
     public static String declararNumeroSorte(String signo) {
 
         String result = "";
-        if (signo.equals("Aries")) {
+        if (signo.equals("Áries")) {
             result = " 8, 19, 25, 33, 41 e 56";
         } else if (signo.equals("Touro")) {
             result = " 4, 12, 21, 30, 38 e 47";
-        } else if (signo.equals("gemeos")) {
+        } else if (signo.equals("Gêmeos")) {
             result = "3, 14, 23, 32, 45 e 59";
-        } else if (signo.equals("cancer")) {
+        } else if (signo.equals("Cancêr")) {
             result = " 10, 16, 27, 36, 49 e 52";
-        } else if (signo.equals("leao")) {
+        } else if (signo.equals("Leão")) {
             result = "6, 18, 29, 37, 44 e 55";
-        } else if (signo.equals("virgem")) {
+        } else if (signo.equals("Virgem")) {
             result = "7, 15, 24, 35, 46 e 58";
-        } else if (signo.equals("libra")) {
+        } else if (signo.equals("Libra")) {
             result = " 2, 13, 22, 31, 40 e 51";
-        } else if (signo.equals("escorpiao")) {
+        } else if (signo.equals("Escorpião")) {
             result = " 5, 17, 26, 34, 43 e 57";
-        } else if (signo.equals("sagitario")) {
+        } else if (signo.equals("Sagitário")) {
             result = " 9, 20, 28, 39, 48 e 60";
-        } else if (signo.equals("capricornio")) {
+        } else if (signo.equals("Capricórnio")) {
             result = " 1, 11, 31, 42, 53 e 59";
-        } else if (signo.equals("aquario")) {
+        } else if (signo.equals("Aquário")) {
             result = " 11, 22, 30, 48, 50 e 54";
-        } else {
+        } else if (signo.equals("Peixes")) {
             result = " 15, 26, 34, 45, 57 e 60";
 
         }
